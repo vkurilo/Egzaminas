58ccba15f686c1677ce8296b4f948b81911d1407
vkurilo <92544044+vkurilo@users.noreply.github.com>
2023-03-05 19:59:54 +0200

    validacijos done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const headerName = document.querySelector("#first-name");
+const headerLast = document.querySelector("#last-name");
+const headerPhone = document.querySelector("#phone-number");
+const headerButton = document.querySelector('#get-started')
+const monthly = document.querySelector('#monthly')
+const yearly = document.querySelector('#yearly')
 
+
+
+headerButton.addEventListener('click', () => { 
+    if(headerName.value && headerLast.value && headerPhone.value && (monthly.checked || yearly.checked ) ){
+        confirm("Your trial started")
+        headerName.value = ""
+        headerLast.value = ""
+        headerPhone.value = ""
+    }else{
+        alert("Enter all inputs")
+    }
+
+   
+})
